f2102a001a232d17dfa661e003ee736b671840f1
Awszome-Ar11 <743241@student.dusd.net>
2026-02-04 11:40:33 -0800

    Finished project
    Added camera script, changed color, made car controlable

--- P4PlayerControlAltamirano/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlAltamirano/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forwards
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
